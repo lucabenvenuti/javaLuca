30b5d209acfb9c9e2d285c94a384e22db4be3a45
Luca Benvenuti <luca.benvenuti@jku.at>
2014-10-27 11:39:56 +0100

    swe1_a03ex finished

--- SWE1_A03ex/src/LoveIndex.java
+++ SWE1_A03ex/src/LoveIndex.java
@@ -1,14 +1,100 @@
-import java.util.Arrays;
-
-//package stem.out;
-
-//import Input;
-
 public class LoveIndex {
 	public static void main(String[] args) { 
-		/*char charTest = 'b', charTest2 = 'B';
-		int Bis = charTest - charTest2;
-		System.out.println(Bis);*/
+		char charRead;
+		int char2int = 0,  i = 0, j = 0, sumFirstName = 0, sum = 0, rest = 0, sumSecondName = 0;
+		
+		System.out.print("Insert your name and the name of your partner (separation by one or more spaces): "); 
+		
+	    charRead = Input.readCharSequence();
+		char2int = (int) charRead;
+
+		while (char2int != 10) {
+			
+			if (char2int == 32 & j==0){
+			}
+			else if (char2int == 32 & j==1){
+				charRead = Input.readCharSequence();
+				char2int = (int) charRead;
+				i=i+1;
+				break;
+			}
+			else if (char2int <= 122 & char2int >= 97 ){
+				char2int = char2int-32;
+				sumFirstName = sumFirstName + char2int;
+				j=1;
+			}
+			else if (char2int <= 90 & char2int >= 65 ){
+				sumFirstName = sumFirstName + char2int;
+				j=1;
+			}
+			else{
+				System.out.print("Invalid input detected");
+				return;
+			}
+			charRead = Input.readCharSequence();
+			char2int = (int) charRead;
+			i=i+1;
+		}
+
+		if (sumFirstName == 0){
+			System.out.print("The love index can only be calculated for 2 names");
+			return;
+		}
+		
+		j = 0;
+		
+		while (char2int != 10) {
+			
+			if (char2int == 32 & j==0){
+			}
+			else if (char2int == 32 & j==1){
+				break;
+			}
+			else if (char2int <= 122 & char2int >= 97 ){
+				char2int = char2int-32;
+				sumSecondName = sumSecondName + char2int;
+				j=1;
+			}
+			else if (char2int <= 90 & char2int >= 65 ){
+				sumSecondName = sumSecondName + char2int;
+				j=1;
+			}
+			else{
+				System.out.print("Invalid input detected");
+				return;
+			}
+			charRead = Input.readCharSequence();
+			char2int = (int) charRead;
+			i=i+1;
+		}
+		
+		if (sumSecondName == 0){
+			System.out.print("The love index can only be calculated for 2 names");
+			return;
+		}
+		
+		sum = sumFirstName + sumSecondName;
+		rest = sum % 101;
+		
+		System.out.printf("Love index: %d %% \n", rest); 
+  }
+
+}
+
+
+/*		System.out.println("Value of obj: " + Arrays.toString(array1));
+System.out.println("Value of obj2: " + array1[1]);
+System.out.println("Value of obj2: " + array1[2]);
+System.out.println("Value of obj2: " + sumFirstName);
+System.out.println("Value of obj2: " + sumSecondName);
+System.out.println("Value of obj2: " + sum);
+System.out.println("Value of obj2: " + rest); */
+
+
+/*
+import java.util.Arrays;
+
+
 		int[] array1 = new int[50];
 		
 		System.out.print("Please enter your height in cm (<=0 for exit): "); 
@@ -18,7 +104,6 @@ public class LoveIndex {
 		int i = 0;
 		int j = 0;
 		int sumFirstName = 0;
-//		System.out.println("Value of obj: " + test2);
 		while (char2int != 10) {
 			
 			if (char2int == 32 & j==0){
@@ -35,21 +120,17 @@ public class LoveIndex {
 			else if (char2int <= 122 & char2int >= 97 ){
 				char2int = char2int-32;
 				sumFirstName = sumFirstName + char2int;
-			//	i=i+1;
 				j=1;
 			}
 			else if (char2int <= 90 & char2int >= 65 ){
 				sumFirstName = sumFirstName + char2int;
-			//	i=i+1;
 				j=1;
 			}
 			else{
 				System.out.print("Invalid input detected");
 				return;
 			}
-			
-			//end
-			
+
 			array1[i] = char2int;
 			charRead = Input.readCharSequence();
 			char2int = (int) charRead;
@@ -88,8 +169,6 @@ public class LoveIndex {
 				return;
 			}
 			
-			//end
-			
 			array1[i] = char2int;
 			charRead = Input.readCharSequence();
 			char2int = (int) charRead;
@@ -106,14 +185,7 @@ public class LoveIndex {
 		
 		int rest = sum % 101;
 		
-/*		System.out.println("Value of obj: " + Arrays.toString(array1));
-		System.out.println("Value of obj2: " + array1[1]);
-		System.out.println("Value of obj2: " + array1[2]);
-		System.out.println("Value of obj2: " + sumFirstName);
-		System.out.println("Value of obj2: " + sumSecondName);
-		System.out.println("Value of obj2: " + sum);
-		System.out.println("Value of obj2: " + rest); */
-		System.out.printf("Love index: %d %% \n", rest); 
-  }
 
-}
+		System.out.printf("Love index: %d %% \n", rest); 
+
+*/
--- SWE1_A03ex/src/birthdayCalendar.java
+++ SWE1_A03ex/src/birthdayCalendar.java
@@ -1,25 +1,19 @@
-//import java.util.Date;
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
 public class birthdayCalendar {
 
 	public static void main(String[] args) {
-		int year = 0, month = 0, days = 0, daysMax = 0, m = 0, x = 0, y = 0, d = days, yearPrint = 0, age =0 , weekday = 0, dateCheck =0, yearPrintStore = 0, ageRest = 0;
+		int year = 0, month = 0, days = 0, daysMax = 0, m = 0, x = 0, y = 0, d = 0,  yearPrint = 0, age =0 , weekday = 0, dateCheck =0, yearPrintStore = 0, ageRest = 0;
 		int yearNow = Calendar.getInstance().get(Calendar.YEAR);
 		int monthNow = Calendar.getInstance().get(Calendar.MONTH);
 		monthNow = monthNow + 1; //It's just part of the horrendous mess which is the Java date/time API
 		int dayNow = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
 		boolean leapyear; // = true;
-//		boolean birthdayToday = false; // = true;
 		String dayOfTheWeek = null;
 		
-		System.out.print("Please enter your birth year: "); 
+		System.out.print("Please enter your birth year (YYYY): "); 
 		year = Input.readInt(); 
-
-
 		
 		if (year < 2015 & year > 1582){
 			leapyear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
@@ -29,7 +23,7 @@ public class birthdayCalendar {
 			return;
 		}
 		
-		System.out.print("Please enter your birth month: "); 
+		System.out.print("Please enter your birth month (M or MM, 0..12): "); 
 		month = Input.readInt(); 
 		
 		if (month <= 12 & month >0){
@@ -56,13 +50,13 @@ public class birthdayCalendar {
 			daysMax = 31;
 		}
 
-		System.out.print("Please enter your birth day: "); 
+		System.out.print("Please enter your birth day (D or DD, 0..31): "); 
 		days =  Input.readInt(); 
 		
 		if (days <= daysMax & days >0){
 		}
 		else{
-			System.out.print("Invalid days detected");
+			System.out.print("Invalid number of days for this month");
 			return;
 		}
 
@@ -88,7 +82,6 @@ public class birthdayCalendar {
 		}
 		d = days;
 		weekday = (x + d + (31*m)/12 + (5*y)/4 - (3*(1+y/100)/4) ) % 7 ;
-	//	System.out.printf("Error made: %.8f \n", weekday); 
 		
 		switch (weekday) {
 		case 0: dayOfTheWeek = "Saturday"; break;
@@ -99,19 +92,9 @@ public class birthdayCalendar {
 		case 5: dayOfTheWeek = "Thursday";break;
 		case 6: dayOfTheWeek = "Friday";break;
 		}
-		
-
-	/*	System.out.printf("Error made: %d \n", yearNow); 
-		System.out.printf("Error made: %d \n", monthNow); 
-		System.out.printf("Error made: %d \n", dayNow); 
-		System.out.printf("Error made: %d \n", weekday); */
-		
-
-		
+	
 		age = yearPrint - year;
-		
-	//	assessment = "hot like hell";
-		
+
 		switch (dateCheck) {
 		case 1: { System.out.println("Today is your birthday. Congratulations! Today is: "  + dayOfTheWeek + " and your age is: " + age );
 		break;}//birthday today
@@ -124,14 +107,10 @@ public class birthdayCalendar {
 		}
 		
 		yearPrintStore = yearPrint;
-		
 		ageRest = age/10;
-	//	int ageNew = ageRest * 10 + 10;
 		
 		for (int ageNew = ageRest * 10 + 10 ; ageNew < 101; ageNew = ageNew+10) {
-			yearPrint = year + ageNew;
-	//	    System.out.print("value of x : " + ageNew );
-	//	    System.out.print("\n");
+
 			yearPrint = ageNew + year;
 		    y = yearPrint;
 		    weekday = (x + d + (31*m)/12 + (5*y)/4 - (3*(1+y/100)/4) ) % 7 ;
@@ -153,3 +132,20 @@ public class birthdayCalendar {
 	}
 
 }
+
+//	int ageNew = ageRest * 10 + 10;
+
+/*	System.out.printf("Error made: %d \n", yearNow); 
+	System.out.printf("Error made: %d \n", monthNow); 
+	System.out.printf("Error made: %d \n", dayNow); 
+	System.out.printf("Error made: %d \n", weekday); 
+				yearPrint = year + ageNew;
+	*/
+
+//	    System.out.print("value of x : " + ageNew );
+//	    System.out.print("\n");
+
+
+//	assessment = "hot like hell";
+
+	
