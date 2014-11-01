64b8469e93d13793e3df077c0292cf4e14332ab2
Luca Benvenuti <luca.benvenuti@jku.at>
2014-11-01 19:52:27 +0100

    almost finished exer 1, only input is missing

--- SWE1_A04ex/src/RGBtoHSB.java
+++ SWE1_A04ex/src/RGBtoHSB.java
@@ -4,130 +4,51 @@ import java.util.Calendar;
 public class RGBtoHSB {
 
 	public static void main(String[] args) {
-		int year = 0, month = 0, days = 0, daysMax = 0, m = 0, x = 0, y = 0, d = 0,  yearPrint = 0, age =0 , weekday = 0, dateCheck =0, yearPrintStore = 0, ageRest = 0;
-		int yearNow = Calendar.getInstance().get(Calendar.YEAR);
-		int monthNow = Calendar.getInstance().get(Calendar.MONTH);
-		monthNow = monthNow + 1; //It's just part of the horrendous mess which is the Java date/time API
-		int dayNow = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-		boolean leapyear; // = true;
-		String dayOfTheWeek = null;
+		int Rinput = 75, Ginput = 0, Binput = 130;
+		double H = 0.0, S = 0.0, V = 0.0, R = 0.0, G = 0.0, B = 0.0, delta = 0.0, cMax = 0.0, cMin = 0.0;
 		
-		System.out.print("Please enter your birth year (YYYY): "); 
-		year = Input.readInt(); 
+		//input
 		
-		if (year < (yearNow) && year > 1582){
-			leapyear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
-		}
-		else{
-			System.out.println("Invalid year! (1582 <= YYYY < " + (yearNow) + ")");
-			return;
-		}
+		R = (double) Rinput; 
+		G = (double) Ginput; 
+		B = (double) Binput; 
 		
-		System.out.print("Please enter your birth month (M or MM, 0..12): "); 
-		month = Input.readInt(); 
+		cMax = Math.max(R,G);
+		if (B>cMax) cMax = B;
 		
-		if (month <= 12 && month >0){
-		}
-		else{
-			System.out.print("Invalid month!");
-			return;
-		}
+		cMin = Math.min(R,G);
+		if (B<cMin) cMin = B;
 		
-		switch (month) {
-		case 9:
-		case 4:
-		case 6:
-		case 11:
-			daysMax = 30;
-			break;
-		case 2:
-			if (leapyear)
-				daysMax = 29;
-			else
-				daysMax = 28;
-			break;
-		default:
-			daysMax = 31;
-		}
-
-		System.out.print("Please enter your birth day (D or DD, 0..31): "); 
-		days =  Input.readInt(); 
+		delta = cMax - cMin;
 		
-		if (days <= daysMax && days >0){
-		}
-		else{
-			System.out.print("Invalid number of days for this month!");
-			return;
-		}
-
-		if (monthNow > month ){
-			yearPrint = yearNow +1;
-			dateCheck = 2;
-		} else if (monthNow == month){
-			if (dayNow > days){yearPrint = yearNow +1;; dateCheck = 2;}
-			else if (dayNow == days){yearPrint = yearNow; dateCheck = 1;}
-			else{yearPrint = yearNow;}	
-		}
-		else {yearPrint = yearNow;}
+		System.out.format("The of thee is " + "%.3f, while the value of "
+				+ "the integer variable is %.3f, and the string is %.3f \n", delta, cMin, cMax); 
 		
-		if (month >= 3){
-			m = month - 2;
-			x = 8;
-			y = yearPrint;
-			}
-		else{
-			m = month;
-			x = 13;
-			y = yearPrint - 1;
-		}
-		d = days;
-		weekday = (x + d + (31*m)/12 + (5*y)/4 - (3*(1+y/100)/4) ) % 7 ;
 		
-		switch (weekday) {
-		case 0: dayOfTheWeek = "Saturday"; break;
-		case 1: dayOfTheWeek = "Sunday"; break;
-		case 2: dayOfTheWeek = "Monday";break;
-		case 3: dayOfTheWeek = "Tuesday";break;
-		case 4: dayOfTheWeek = "Wednesday";break;
-		case 5: dayOfTheWeek = "Thursday";break;
-		case 6: dayOfTheWeek = "Friday";break;
-		}
-	
-		age = yearPrint - year;
-
-		switch (dateCheck) {
-		case 1: { System.out.println("Today is your birthday. Congratulations! Today is: "  + dayOfTheWeek + " and your age is: " + age );
-		break;}//birthday today
-		case 2: { 
-			System.out.println("Your birthday in " + yearNow + " has already passed; birthday in " + yearPrint + " will be on "  + dayOfTheWeek + " and your age will be: " + age );
-			break;} //birthday next year
-
-		default:
-			System.out.println("Your birthday in " + yearPrint + " will be on: "  + dayOfTheWeek + " and you will be: " + age ); //, yearNow); 
-		}
 		
-		yearPrintStore = yearPrint;
-		ageRest = age/10;
+		if (cMax==0.0){ //H = 0
+		} else if (cMax==R){
+			if ((G-B)==0.0 && delta==0.0){H = 0.0;} // (double)(60+360)%360;}
+			else {
+			H = (60*((G-B)/delta)+360)%360;}
+		} else if (cMax==G){
+			if ((B-R)==0.0 && delta==0.0){H = 0.0;} // = 180.0;}
+			else {
+				H = 60*(B-R)/delta + 120;}
+		} else if (cMax==B){
+			if ((R-G)==0.0 && delta==0.0){H = 0.0;} // = 300.0;}
+			else {
+				H = 60*(R-G)/delta + 240;}			
+		} else {System.out.format("trotaculo");}
 		
-		for (int ageNew = ageRest * 10 + 10 ; ageNew < 101; ageNew = ageNew+10) {
-
-			yearPrint = ageNew + year;
-		    y = yearPrint;
-		    weekday = (x + d + (31*m)/12 + (5*y)/4 - (3*(1+y/100)/4) ) % 7 ;
-		    
-			switch (weekday) {
-			case 0: dayOfTheWeek = "Saturday"; break;
-			case 1: dayOfTheWeek = "Sunday"; break;
-			case 2: dayOfTheWeek = "Monday";break;
-			case 3: dayOfTheWeek = "Tuesday";break;
-			case 4: dayOfTheWeek = "Wednesday";break;
-			case 5: dayOfTheWeek = "Thursday";break;
-			case 6: dayOfTheWeek = "Friday";break;
-			}
-			yearPrint = ageNew + year;
-			if (yearPrint!= yearPrintStore){
-			System.out.println("Your birthday in " + yearPrint + " will be on: "  + dayOfTheWeek + " and you will be: " + ageNew);}
-		 }
+		if (delta==0.0 && cMax ==0.0){S = 0.0;}
+		else {S = delta/cMax;}
+		
+		V = cMax/255.0;
+	//	H = (((45.0)/(117.0))%6)*60;
+		System.out.format("The of thee is " + "%.3f, while the value of "
+				+ "the integer variable is %.3f, and the string is %.3f", H,S,V); 
+		
 
 	}
 
