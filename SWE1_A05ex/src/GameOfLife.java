8366a3ffe67232aae27f50d8b6a42e11664ac901
Luca Benvenuti <luca.benvenuti@jku.at>
2014-11-15 18:28:51 +0100

    sono riuscito a importare la matrice dio porco

--- SWE1_A05ex/src/GameOfLife.java
+++ SWE1_A05ex/src/GameOfLife.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 
 
 public class GameOfLife {
@@ -8,59 +9,52 @@ public class GameOfLife {
 	//public static void readFile(String path) throws IOException {
 	public static int[][] readFile(String path) throws IOException {
 		//comment: method incomplete and needs to be extended!
-		int a[][] = new int [20][20];
+
 		BufferedReader buffRead = new BufferedReader(new FileReader(path));
 		String line = buffRead.readLine();
+		int[][] arrayNumbers = new int [10][10];
+		int ira = 0, j= 0;
+		int a =0;
+		
 		while (line != null) {
 		//split a line into characters
 		char[] parts = line.toCharArray();
-		char c = 'a';
-		int value = 0, charValue;
-	/*	System.out.print("B: "); 
 		
-		//int lengthParts = parts.length;
-		System.out.println(parts[0]);
-		System.out.print("C: "); 
-		System.out.println(parts[1]); */
-		int i = 0;
-		int j = 0;
-	//	for(char s:parts){
-	//	for (int i = 0;i<11; i++){
+//		System.out.println(Arrays.toString(parts));
+	/*	System.out.print("BX: "); 
+		System.out.println("The char is " + parts[2]);
+		System.out.print("CX: "); 
 		
-		while (i < parts.length){
-	//	a[i][j] =	Character.getNumericValue(s);
+		System.out.format("H: %d", parts.length, "\n"); */
+				
+		for(char s:parts){
 		// use Character.getNumericValue(s) to transform a character
 		// 's' into a numeric value and populate the initial matrix
-		//	System.out.println(s);
+		a =	Character.getNumericValue(s);
 		
-		if (c < '0' || c > '9') break;     // if it isn't a number: stop
-		charValue = (int)c - (int)'0';     // convert numeric character to number
-		value = 10 * value + charValue;  
-		
-			System.out.format(" (%d) \n", a[i][j]);
+		if (a==-1){}
+		else {
+					
+			arrayNumbers[j][ira] = a;
 			
-			i = i+1;
+			System.out.format(" (%d) \n", a);
+			ira = ira+1;
+			}
 		}
-		System.out.println("Number: " + value);
 		//read next line
+		System.out.format("trota \n ");
 		line = buffRead.readLine();
-		j = j +1;
+		j = j+1;
+		ira = 0;
 		}
-		buffRead.close();
 		
-/*		for (int k = 0; k < a.length; k++) {
-		    for (int l = 0; l < a.length; l++) {
-		        System.out.print(a[k][l] + " ");
+		for (int k = 0; k < 10; k++) {
+		    for (int l = 0; l < 10; l++) {
+		        System.out.print(arrayNumbers[k][l] + " ");
 		    }
 		    System.out.print("\n");
 		}
-		*/
-	//	System.out.format(" (%d) \n", a[2]);
-	//	System.out.print("CX: "); 
-	//	System.out.println(a[2][1]);
-	/*	System.out.println(a[3][2]);
-		System.out.println(a[13][1]);
-		System.out.println(a[8][3]); */
+		
 		int b[][] = {{0, 0 },{ 0, 0}};
 		return b;
 		
