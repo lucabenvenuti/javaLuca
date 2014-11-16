d517333d1512766258de8d9c5ad03b826f83d15d
Luca Benvenuti <luca.benvenuti@jku.at>
2014-11-16 12:04:13 +0100

    started working on memory game

---
+++ SWE1_A05ex/src/MemoryGame.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
+
+public class MemoryGame {
+	
+	
+
+	
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void main(String[] args) {
+		int a=0 , countAlive =0 , jj=0 , value = 0;
+		boolean live = true;		
+		
+		
+		System.out.print("CXX: "); 
+		
+		
+		
+
+		
+}
+}
+
+
+/*		switch (month) {
+		case 9:
+		case 4:
+		case 6:
+		case 11:
+			daysMax = 30;
+			break;
+		case 2:
+			if (leapyear)
+				daysMax = 29;
+			else
+				daysMax = 28;
+			break;
+		default:
+			daysMax = 31;
+		}*/
