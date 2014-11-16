d61efb6dd3bc77afde1a0688a904c8cc47fbe62d
Luca Benvenuti <luca.benvenuti@jku.at>
2014-11-16 13:36:26 +0100

    the code makes sense, need to be tested

--- SWE1_A05ex/src/MemoryGame.java
+++ SWE1_A05ex/src/MemoryGame.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
@@ -6,34 +7,102 @@ import java.util.Arrays;
 
 public class MemoryGame {
 	
-	
+
+	  // Implementing Fisher–Yates shuffle
+	  static void shuffleArray(int[] ar)
+	  {
+	    Random rnd = new Random();
+	    for (int i = ar.length - 1; i > 0; i--)
+	    {
+	      int index = rnd.nextInt(i + 1);
+	      // Simple swap
+	      int a = ar[index];
+	      ar[index] = ar[i];
+	      ar[i] = a;
+	    }
+	  }
 
 	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		//int a=0 , countAlive =0 , jj=0 , value = 0;
-		//boolean live = true;		
+		boolean newGen = false;		
 		
-		int [][] arrayNumbers = new int [20][20];
+		int [][] arrayNumbers = new int [20][2];
+		int counter =0, counter2 =0;
 		
 		int row = arrayNumbers.length;
 		int col = arrayNumbers[0].length;
 		int [][] arrayNumbersNew = new int [row][col];
+		int [] arrayStorage = new int [row*col];
 		
 		double x = Math.random()*77;
-		int y = (int) (Math.round(x)+48);		
+		int y = (int) (Math.round(x)+48);	
+		
+		double rowPos = Math.random()*row;
+		int rowPos2 = (int) (Math.round(rowPos));	
 		
 		System.out.print("CXX test: \n"); 
 		System.out.format(" loop number %.3f (%d) ", x, y);
 		
-		for (int line1 = 0; line1 < 10; line1++) {
-		    for (int column1 = 0; column1 < 10; column1++) {
-		
-		    	arrayNumbers[line1][column1] = 0;
+		for (int line1 = 0; line1 < row; line1++) {
+		    for (int column1 = 0; column1 < col; column1++) {
+		    	
+		    	if (counter<=(row*col/2) && arrayNumbers[line1][column1] ==0 )
+		    	{
+		    		//until it is true the while goes
+		    		// I want to continue until I get a random that is not in the list //!newGen
+		    		counter2 = 0;
+		    		while (newGen){
+		    			
+		    			if (counter2 > 1000){break;}
+		    			
+			    		newGen = false;
+			    		x = Math.random()*77;
+			    		y = (int) (Math.round(x)+48);	
+			    		
+			    		for (int k = 0; k < arrayStorage.length; k++) {
+			    			if (y == arrayStorage[k]){
+			    				newGen = true;
+			    			
+			    			}else {}	
+			    		}
+		    			counter2 = counter+1;
+		    		}
+		    		counter2 = 0;
+		    		newGen = false;
+			    	arrayNumbers[line1][column1] = y;
+			    	arrayStorage[counter]=y;
+			    	
+			    	counter = counter +1;
+
+		    	}
+
+		    	else if (counter>(row*col/2) && arrayNumbers[line1][column1] ==0 )
+		    	{
+		    	break;}
+		    	else {break;}
+		    	
 		    	arrayNumbers[line1][column1] = arrayNumbersNew[line1][column1];
 		    	arrayNumbersNew[line1][column1] = 0;
 		    }
+		    
+		    if (counter>(row*col/2) )
+	    	{
+	    	break;}
+		    
 		}
+		
+		
+		
+		 int[] solutionArray = { 1, 2, 3, 4, 5, 6, 16, 15, 14, 13, 12, 11 };
+
+		    shuffleArray(solutionArray);
+		    for (int i = 0; i < solutionArray.length; i++)
+		    {
+		      System.out.print(solutionArray[i] + " ");
+		    }
+		    System.out.println();
 		
 
 		
@@ -56,4 +125,12 @@ public class MemoryGame {
 			break;
 		default:
 			daysMax = 31;
-		}*/
+		}
+		    	if (newGen){	
+}
+		    	else{}
+		    	
+		    	}		
+		*
+		*
+		*/
