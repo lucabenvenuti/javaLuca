ba4424432a04e2a00ef636b75611a9171bee2d36
lucabenvenuti <luca.benvenuti@jku.at>
2015-03-30 19:45:41 +0200

    continue commenting

--- SWE2_A04ex/src/first/Filter.java
+++ SWE2_A04ex/src/first/Filter.java
@@ -5,6 +5,22 @@ import java.util.function.Predicate;
 
 import second.Receiver;
 
+/**
+ * Filter.java
+ *
+ * A {@link Filter} is a generic class which implements a Receiver interface;
+ * next Receiver can be set after instantiation.
+ * The remaining fields (Consumer<A> onReceive, Predicate<A> predicate,
+			Consumer<A> onSend) 
+ * are for the high level method receive(A a).
+ * 
+ * Software Development II, 2015SS
+ * JKU Linz
+ * 
+ * @author Luca Benvenuti
+ * 
+ * @param <A>
+ */
 public class Filter<A> implements Receiver<A> {
 
 	private Receiver<A> next;
@@ -13,27 +29,39 @@ public class Filter<A> implements Receiver<A> {
 	private final Predicate<A> predicate;
 	private final Consumer<A> onSend;
 
+	/**
+	 * @param onReceive
+	 * @param predicate
+	 * @param onSend
+	 */
 	public Filter(Consumer<A> onReceive, Predicate<A> predicate,
 			Consumer<A> onSend) {
-		super();
 		this.onReceive = onReceive;
 		this.predicate = predicate;
 		this.onSend = onSend;
 	}
 
 
+	/**
+	 * @param next
+	 */
 	public void setNext(Receiver<A> next) {
 		this.next = next;
 	}
 	
 	
+	/** (non-Javadoc)
+	 * @see second.Receiver#receive(java.lang.Object)
+	 * predicate.test(a)) applies the filter function.
+	 * next.receive(a) left.receive(a) passes the value to the whatEver component
+	 */
 	@Override
 	public void receive(A a) {
 		onReceive.accept(a);
-		if (predicate.test(a)) { // apply the filter function
+		if (predicate.test(a)) { 
 			onSend.accept(a);
 			if (next != null) {
-				next.receive(a); // pass the value to the whatEver component
+				next.receive(a);
 			}
 
 		}
