8afce03d18e2eb3eea4d0ba67878368ef42457e5
lucabenvenuti <luca.benvenuti@jku.at>
2015-04-24 12:40:38 +0200

    second

--- SWE2_A06ex/src/textana/First.java
+++ SWE2_A06ex/src/textana/First.java
@@ -1,12 +1,13 @@
-package test;
+package textana;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class First {
 
 	public static void main(String[] args) {
 	try {
-		java.nio.file.Files.lines(java.nio.file.Paths.get("sampletext.txt"));
+		Stream<String> lines = java.nio.file.Files.lines(java.nio.file.Paths.get("sampletext.txt"));
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
