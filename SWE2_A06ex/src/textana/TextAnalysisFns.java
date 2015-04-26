8844477e9704d7b73e9e29e38300c6cb79a383e7
lucabenvenuti <luca.benvenuti@jku.at>
2015-04-26 20:37:22 +0200

    looks finished

--- SWE2_A06ex/src/textana/TextAnalysisFns.java
+++ SWE2_A06ex/src/textana/TextAnalysisFns.java
@@ -1,41 +1,53 @@
 package textana;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.function.Function;
-import java.util.function.IntSupplier;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.StreamSupport;
 
 public class TextAnalysisFns {
 
-	// (a) find lines which contains a particular word
-
+	/**
+	 * @param fileName
+	 * @param word
+	 * @return
+	 * @throws IOException
+	 *             (a) find the first line which contains a particular word
+	 */
 	public static Optional<String> findLine(String fileName, String word)
 			throws IOException {
 		return Files.lines(Paths.get(fileName)).filter(l -> l.contains(word))
 				.findFirst();
 	}
 
+	/**
+	 * @param fileName
+	 * @param word
+	 * @return
+	 * @throws IOException
+	 *             (a) find lines which contain a particular word
+	 */
 	public static List<String> findLines(String fileName, String word)
 			throws IOException {
 		return Files.lines(Paths.get(fileName)).filter(l -> l.contains(word))
 				.collect(Collectors.toList());
 	}
 
-	// (b) find all line numbers which contain a text and return them in a list
-
+	/**
+	 * @param fileName
+	 * @param word
+	 * @return
+	 * @throws IOException
+	 *             (b) find the number of the line which contains a text and
+	 *             return as an optional
+	 */
 	public static Optional<Integer> findLineNum(String fileName, String word)
 			throws IOException {
 
@@ -53,6 +65,14 @@ public class TextAnalysisFns {
 				.filter(p -> p.scd.contains(word)).map(x -> x.fst).findFirst();
 	}
 
+	/**
+	 * @param fileName
+	 * @param word
+	 * @return
+	 * @throws IOException
+	 *             (b) find all line numbers which contain a text and return
+	 *             them in a list
+	 */
 	public static List<Integer> findLineNums(String fileName, String word)
 			throws IOException {
 		Supplier<Integer> counter = new Supplier<Integer>() {
@@ -70,73 +90,92 @@ public class TextAnalysisFns {
 				.collect(Collectors.toList());
 	}
 
-	// (c) all words
-
-	public static boolean isNumeric(String str) {
-		return str.matches("-?\\d+(\\.\\d+)?"); // match a number with optional
-												// '-' and decimal.
-	}
-
+	/**
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 *             (c) all words
+	 */
 	public static List<String> words(String fileName) throws IOException {
 		return Files.lines(Paths.get(fileName)).map(line -> line.toLowerCase())
-				.map(line -> line.replaceAll("[^a-zA-Z ]", ""))
+				.map(line -> line.replaceAll("[^a-z ]", ""))
 				.map(line -> line.split(" ")).flatMap(Arrays::stream)
-				.filter(w -> !isNumeric(w)).collect(Collectors.toList());
+				.filter(w -> !w.equals("")).collect(Collectors.toList());
 	}
 
-	// (d) word occurrences
-
+	/**
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 *             (d) word occurrences
+	 */
 	public static Map<String, Integer> wordOccurrences(String fileName)
 			throws IOException {
 
 		return Files
 				.lines(Paths.get(fileName))
 				.map(line -> line.toLowerCase())
-				.map(line -> line.replaceAll("[^a-zA-Z ]", ""))
+				.map(line -> line.replaceAll("[^a-z ]", ""))
 				.map(line -> line.split(" "))
 				.flatMap(Arrays::stream)
+				.filter(w -> !w.equals(""))
 				.collect(
 						Collectors.groupingBy(o -> o,
 								Collectors.reducing(0, e -> 1, Integer::sum)));
 	}
 
-	// (e) set of words
-
+	/**
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 *             (e) set of words
+	 */
 	public static Set<String> setOfWords(String fileName) throws IOException {
-		// TODO
-		return new TreeSet<>
-		(Files.lines(Paths.get(fileName))
-				.map(line -> line.toLowerCase())
-				.map(line -> line.replaceAll("[^a-zA-Z ]", ""))
+
+		return Files.lines(Paths.get(fileName)).map(line -> line.toLowerCase())
+				.map(line -> line.replaceAll("[^a-z ]", ""))
 				.map(line -> line.split(" ")).flatMap(Arrays::stream)
-				.collect(Collectors.toSet()));
+				.filter(w -> !w.equals(""))
+				.collect(Collectors.toCollection(() -> new TreeSet<>()));
 	}
 
-	// (f) grouping alphabetically and by length
-
+	/**
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 *             (f) grouping words alphabetically
+	 */
 	public static Map<Character, List<String>> groupByFirstChar(String fileName)
 			throws IOException {
 		return Files
 				.lines(Paths.get(fileName))
 				.map(line -> line.toLowerCase())
-				.map(line -> line.replaceAll("[^a-zA-Z ]", ""))
+				.map(line -> line.replaceAll("[^a-z ]", ""))
 				.map(line -> line.split(" "))
 				.flatMap(Arrays::stream)
+				.filter(w -> !w.equals(""))
 				.collect(
 						Collectors.groupingBy(w -> w.length() > 0 ? w.charAt(0)
 								: 0, Collectors.mapping(w -> w,
 								Collectors.toList())));
 	}
 
+	/**
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 *             (f) grouping words by length
+	 */
 	public static Map<Integer, List<String>> groupByLength(String fileName)
 			throws IOException {
-		// TODO
+
 		return Files
 				.lines(Paths.get(fileName))
 				.map(line -> line.toLowerCase())
-				.map(line -> line.replaceAll("[^a-zA-Z ]", ""))
+				.map(line -> line.replaceAll("[^a-z ]", ""))
 				.map(line -> line.split(" "))
 				.flatMap(Arrays::stream)
+				.filter(w -> !w.equals(""))
 				.collect(
 						Collectors.groupingBy(w -> w.length(),
 								Collectors.mapping(w -> w, Collectors.toList())));
