e3dd2d43a0bf1794f223d4a65f2fd23e60ec691d
lucabenvenuti <luca.benvenuti@jku.at>
2015-05-02 14:15:40 +0200

    to do

--- SWE2_A07ex/src/test/ProducerConsumerApp.java
+++ SWE2_A07ex/src/test/ProducerConsumerApp.java
@@ -1,21 +1,37 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class Buffer {
 
-	private Object obj = null;
+	private String[] stringArray = new String[3];
 
-	public synchronized void put(Object o) {
-		obj = o;
+	public synchronized void put(String s) {
+		if (stringArray[0] == null) {
+			stringArray[0] = s;
+		} else if (stringArray[1] == null) {
+			stringArray[1] = s;
+		}
+		// else if (stringArray[2]==null){stringArray[2] = s;}
+		else {
+			stringArray[2] = s;
+		}
+		// stringArray = o;
 	}
 
-	public synchronized Object retrieve() {
-		Object o = obj;
-		obj = null;
+	public synchronized String[] retrieve() {
+		String[] o = stringArray;
+		stringArray[0] = null;
+		stringArray[1] = null;
+		stringArray[2] = null;
 		return o;
 	}
 
 	public synchronized boolean isEmpty() {
-		return obj == null;
+		return stringArray[0] == null && stringArray[1] == null
+				&& stringArray[2] == null;
 	}
 
 }
@@ -39,7 +55,10 @@ class Producer extends Thread {
 						buffer.wait();
 					}
 					String s = this.getId() + " : " + i++;
+					doSomeHeavyWork();
 					buffer.put(s);
+					buffer.put(s + "2");
+					buffer.put(s + "3");
 					System.out.println("Producer produced " + s);
 					buffer.notifyAll();
 				}
@@ -49,6 +68,16 @@ class Producer extends Thread {
 			}
 		}
 		System.out.println("Producer terminated ");
+	}
+
+	private void doSomeHeavyWork() throws InterruptedException {
+		Thread.sleep(10);
+		/*
+		 * BufferedReader br = new BufferedReader(new
+		 * InputStreamReader(System.in)); System.out.print("Enter String"); try
+		 * { String s = br.readLine(); } catch (IOException e) {
+		 * e.printStackTrace(); }
+		 */
 	}
 
 }
@@ -68,8 +97,13 @@ class Consumer extends Thread {
 						System.out.println(" ++ Consumer blocked");
 						buffer.wait();
 					}
-					Object o = buffer.retrieve();
-					System.out.println("  Consumer found " + o);
+					doSomeHeavyWork();
+					String[] o = buffer.retrieve();
+
+					for (int i = 0; i < o.length && o[i] != null; i++) {
+						System.out.println("  Consumer found " + o[0]);
+					}
+					System.out.println("pippo");
 					buffer.notifyAll();
 				}
 				Thread.sleep((int) (100 * Math.random()));
@@ -78,6 +112,10 @@ class Consumer extends Thread {
 			}
 		}
 		System.out.println("Consumer terminated ");
+	}
+
+	private void doSomeHeavyWork() throws InterruptedException {
+		Thread.sleep(10);
 	}
 }
 
@@ -98,7 +136,7 @@ public class ProducerConsumerApp {
 		c2.start();
 
 		try {
-			Thread.sleep(1_000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 		}
 
