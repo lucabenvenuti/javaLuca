5ad2bc40ead30de38fb42c5a7ffb9e4515f585c3
lucabenvenuti <luca.benvenuti@jku.at>
2015-05-02 17:34:08 +0200

    indentation :-(

--- SWE2_A07ex/src/test/Test01.java
+++ SWE2_A07ex/src/test/Test01.java
@@ -6,26 +6,23 @@ import java.io.InputStreamReader;
 
 public class Test01 {
 
-	//public static void main(String[] args) {
-		/*try {
-			int n = Integer.parseInt("123");
-			System.out.println(n);
-		} catch (NumberFormatException e) {
-			System.out.println("not a number");
-		}*/
+	// public static void main(String[] args) {
+	/*
+	 * try { int n = Integer.parseInt("123"); System.out.println(n); } catch
+	 * (NumberFormatException e) { System.out.println("not a number"); }
+	 */
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter String");
+		String s = br.readLine();
+		System.out.print("Enter Integer:");
+		try {
+			int i = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException nfe) {
+			System.err.println("Invalid Format!");
+		}
 
-	    public static void main(String[] args) throws IOException { 
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        System.out.print("Enter String");
-	        String s = br.readLine();
-	        System.out.print("Enter Integer:");
-	        try{
-	            int i = Integer.parseInt(br.readLine());
-	        }catch(NumberFormatException nfe){
-	            System.err.println("Invalid Format!");
-	        }
-		
 	}
 
 }
