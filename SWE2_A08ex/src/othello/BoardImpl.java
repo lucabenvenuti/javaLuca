cfbf3e046232113070290b97478c2c5f3c465ccf
lucabenvenuti <luca.benvenuti@jku.at>
2015-05-09 18:28:54 +0200

    every time you call iterator() starts from 0 again

--- SWE2_A08ex/src/othello/BoardImpl.java
+++ SWE2_A08ex/src/othello/BoardImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 public class BoardImpl implements Board {
 
 	private Map<Pos, Stone> boardMap = new TreeMap<>();
+	private int i = 0;
 
 	/**
 	 * Constructor initializing the board with two white stones on positions D 4
@@ -23,10 +24,20 @@ public class BoardImpl implements Board {
 	 */
 	public BoardImpl() {
 		// TODO
+		
+		for(Pos p:Pos.values()) {
+			boardMap.put(p, Stone.FREE);
+		}
+		boardMap.remove(Pos.D_4);
+		boardMap.remove(Pos.E_5);
+		boardMap.remove(Pos.D_5);
+		boardMap.remove(Pos.E_4);
 		boardMap.put(Pos.D_4, Stone.WHITE);
 		boardMap.put(Pos.E_5, Stone.WHITE);
 		boardMap.put(Pos.D_5, Stone.BLACK);
 		boardMap.put(Pos.E_4, Stone.BLACK);
+		
+		
 	}
 
 	/*
@@ -48,20 +59,49 @@ public class BoardImpl implements Board {
 	 */
 	@Override
 	public void setStone(Pos pos, Stone stone) {
+	//	System.out.println("test");
 		Pos[] validPos = getValidPositions(stone);
-
+		if (validPos == null) {
+			System.out.println("error");
+			return;
+		} else{
+			System.out.println(validPos.length);
+			System.out.println(pos);
+		}
+		
 		for (Pos p : validPos) {
 			if (pos == p) {
 				boardMap.put(pos, stone);
 				// boardMap.remove(key)
-
+				
 				for (Direction dir : Direction.values()) {
+					Pos pos3 = pos.next(dir);
+					
+					List<Pos> candidatesToCapture = new ArrayList<>();
+					if (isValidDirection(pos3, dir)) {
+						//System.out.println(pos==null);
+						if (isFree(pos3)) {// do nothing
+						} else if (stone.isOther(getStone(pos3))) {// do
+																	// nothing
+						} else if (stone == getStone(pos3)) {
+							findCaptureCandidates(candidatesToCapture, pos,
+									stone, dir);
+						} else {
+							System.out.println("Wrong insertion");
+						}
+					}
 
-					while (iterator().hasNext()) {
-						Pos pos2 = iterator().next();
+					capture(candidatesToCapture, stone);
+					
+					
+					
+					/*Iterator iterator1 = iterator();
+					while (iterator1.hasNext()) {
+						Pos pos2 = (Pos) iterator1.next();
 						List<Pos> candidatesToCapture = new ArrayList<>();
 						if (isValidDirection(pos2, dir)) {
-							if (isFree(pos)) {// do nothing
+							System.out.println(pos==null);
+							if (isFree(pos2)) {// do nothing
 							} else if (stone.isOther(getStone(pos2))) {// do
 																		// nothing
 							} else if (stone == getStone(pos2)) {
@@ -71,17 +111,17 @@ public class BoardImpl implements Board {
 								System.out.println("Wrong insertion");
 							}
 						}
-						
+
 						capture(candidatesToCapture, stone);
 
-						/*
+						
 						 * System.out.println(pos2); if (isFree(pos2)) {
 						 * return;// false; }
-						 */
-					}
+						 
+					}*/
 
 				}
-
+		//		System.out.println("test");
 				return;
 			}
 
@@ -94,10 +134,10 @@ public class BoardImpl implements Board {
 
 	private void capture(List<Pos> candidatesToCapture, Stone stone) {
 		// TODO Auto-generated method stub
-		for (Pos p:candidatesToCapture){
+		for (Pos p : candidatesToCapture) {
 			boardMap.remove(p);
-			boardMap.put(p,stone);
-		}		
+			boardMap.put(p, stone);
+		}
 	}
 
 	private void findCaptureCandidates(List<Pos> candidatesToCapture, Pos pos,
@@ -138,9 +178,10 @@ public class BoardImpl implements Board {
 	@Override
 	public boolean isFull() {
 		// TODO
-		while (iterator().hasNext()) {
-			Pos pos = iterator().next();
-			System.out.println(pos);
+		Iterator iterator1 = iterator();
+		while (iterator1.hasNext()) {
+			Pos pos = (Pos) iterator1.next();
+			// System.out.println(pos);
 			if (isFree(pos)) {
 				return false;
 			}
@@ -155,6 +196,7 @@ public class BoardImpl implements Board {
 	 */
 	@Override
 	public Pos[] getValidPositions(Stone stone) {
+		
 		// TODO
 		/*
 		 * Map<Pos, Stone> boardMap2 = boardMap.entrySet().stream() .filter(s ->
@@ -163,34 +205,55 @@ public class BoardImpl implements Board {
 		 * 
 		 * boardMap2.keySet().stream().filter(p -> p.)
 		 */
-
+		
 		Collection<Pos> validPositions = new TreeSet<>();
-
-		while (iterator().hasNext()) {
-			Pos pos = iterator().next();
-			System.out.println(pos);
+	//	System.out.println(validPositions.isEmpty());
+		Iterator iterator1 = iterator();
+		while (iterator1.hasNext()) {
+		//	System.out.println(i++);
+			Pos pos = (Pos) iterator1.next();
+		//	System.out.println(pos.name());
+			// System.out.println(pos);
+		//	System.out.println(pos);
 			if (isFree(pos)) {
 				for (Direction dir : Direction.values()) {
+					//
 					if (!isValidDirection(pos, dir)) {
-						break;
+				//		System.out.println("test1");
+						continue;
 					}
-
+					//System.out.println(pos.next(dir));
+					//System.out.println(dir);
 					// if (getStone(pos).isOther(getStone(pos.next(dir)))
+					//
+				//	System.out.println(getStone(pos.next(dir)).name());
+			//		System.out.println(stone.isOther(getStone(pos.next(dir))));
+				//	System.out.println(getStone(Pos.D_4).isOther(getStone(Pos.D_4.next(Direction.E))));
+					
+					
+			/*		if (stone.isOther(getStone(pos.next(dir)))){
+					//	System.out.println("test3");
+					}*/
+					
+					
 					if (stone.isOther(getStone(pos.next(dir)))
 					// && getStone(pos) == (getStone(pos.next(dir).next(
-							&& stone == (getStone(pos.next(dir).next(dir)))) {
+							&& getStone(pos.next(dir)).isOther(getStone(pos.next(dir).next(dir)))) {
 						validPositions.add(pos);
+					//	System.out.println("test2");
 						break;
 					}
 				}
 			}
 		}
-
+	  //  System.out.println(validPositions.size());
 		return validPositions.toArray(new Pos[validPositions.size()]);
 	}
 
 	public boolean isValidDirection(Pos pos, Direction dir) {
-		return pos.next(dir) != null && pos.next(dir).next(dir) != null;
+		if (pos.next(dir)==null) return false;
+		if (pos.next(dir).next(dir) == null) return false;
+		return true;//pos.next(dir) != null && pos.next(dir).next(dir) != null;
 	}
 
 	/*
